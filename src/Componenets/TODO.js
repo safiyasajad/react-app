a685bd72d08056e79e81449e69a4942a1c70037b
safiyasajad <ssaj0008@student.monash.edu>
2025-12-22 21:27:35 +0300

    Update TODO.js
    Functionality of the button when clicked (shows on console log - no output

--- src/Componenets/TODO.js
+++ src/Componenets/TODO.js
@@ -1,9 +1,15 @@
 function ToDo(props){
+    function deleteHandler(){ //function to handle the listining of the 'delete' button being clicked
+    console.log('Clicked!'); //allows us to know that the fucntionality works (no output)
+    console.log(props.text); //will tell us which of the continers has been clicked
+
+
+    }
     return (
     <div className='card'>
         <h2>{props.text}</h2>   {/* here we are referencing the the text from each of the different blocks in the app.js file. Putting just props.text will output just that, however having it in the {} will allow for it to pick up the reference and put only the variable assigned to it */}
         <div className='actions'>
-          <button className='btn'>Delete</button>
+          <button className='btn' onClick={deleteHandler}>Delete</button> {/*here we will not give a parameter to the deleteHander function as that fucntion will run the parameter as well at the beginneing even when not called  */}
         </div>
       </div>
     );
