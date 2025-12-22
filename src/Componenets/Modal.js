19ad37f83c7a993e739f4791d12f27148ead48f8
safiyasajad <ssaj0008@student.monash.edu>
2025-12-22 21:38:21 +0300

    Update Modal.js
    Content of the delete button

--- src/Componenets/Modal.js
+++ src/Componenets/Modal.js
@@ -1,8 +1,10 @@
 function Modal() {
-    return <div>
+    return <div className="Modal">
         <p>Are you sure?</p>
         <button  className='btn btn--alt'>Cancel</button>
         <button  className="btn">Confirm</button>
     </div>
 }
 export default Modal;
+
+//this is the functionality of the button have to show in the App.js file
