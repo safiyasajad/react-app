1cef4a396c18ec83354135c445cbd8082da15d4b
safiyasajad <ssaj0008@student.monash.edu>
2025-12-22 21:38:47 +0300

    Create Backdrop.js
    backdrop of the container

---
+++ src/Componenets/Backdrop.js
@@ -0,0 +1,8 @@
+//backdrop of the delete button when clicked
+function Backdrop() {
+    return <div className="backdrop"/> //no content between therefore we can write it this way instead of <div></div>
+}
+
+export default Backdrop;
+ 
+//this is the functionality of the backgroud. have to show in the App.js file
